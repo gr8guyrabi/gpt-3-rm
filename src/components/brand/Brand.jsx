91f07672c6c0c4887a33fc1ff0943eed7bb5fad6
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-02-26 16:52:43 +0545

    WIP: setup whatGPT3 section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import * as brands from './imports'
 import './brand.css'
 
 const Brand = () => {
-  return (
-    <div>Brand</div>
+    return (
+    <div className="gpt3__brands section__padding">
+        { Object.keys(brands).map((brand_key, index) => (
+            <div key={brand_key + index}>
+                <img src={brands[brand_key]} alt="brand" />
+            </div>
+        )) }
+    </div>
   )
 }
 
