827cfe784a974ca3a080da8d6a6251ce47fe6e3a
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-02-28 09:53:15 +0545

    create article component and setup blog section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 import './article.css'
 
-const Article = () => {
+const Article = ({ date, title, imgUrl, articleUrl }) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__blog-container_article">
+        <div className="gpt3__blog-container_article-img">
+            <img src={imgUrl} alt="blog_image" />
+        </div>
+        <div className="gpt3__blog-container_article-content">
+            <p>{date}</p>
+            <h3>{title}</h3>
+            <a href={articleUrl} target="__blank" rel="noreferrer">Read Full Article</a>
+        </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 
+import { Article } from '../../components'
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 import './blog.css'
+
+const articlesData = [
+    {
+        date: 'Sep 26, 2021',
+        title: 'GPT-3 and Open  AI is the future. Let us explore how it is?',
+        imgUrl: blog01,
+        articleUrl: '#'
+    },
+    {
+        date: 'Sep 26, 2021',
+        title: 'GPT-3 and Open  AI is the future. Let us explore how it is?',
+        imgUrl: blog02,
+        articleUrl: '#'
+    },
+    {
+        date: 'Sep 26, 2021',
+        title: 'GPT-3 and Open  AI is the future. Let us explore how it is?',
+        imgUrl: blog03,
+        articleUrl: '#'
+    },
+    {
+        date: 'Sep 26, 2021',
+        title: 'GPT-3 and Open  AI is the future. Let us explore how it is?',
+        imgUrl: blog04,
+        articleUrl: '#'
+    },
+    {
+        date: 'Sep 26, 2021',
+        title: 'GPT-3 and Open  AI is the future. Let us explore how it is?',
+        imgUrl: blog05,
+        articleUrl: '#'
+    },
+]
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className="gpt3__blog section__padding" id="blog">
+        <div className="gpt3__blog-heading">
+            <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
+        </div>
+        <div className="gpt3__blog-container">
+            {articlesData.map((article, index) => (
+                <Article
+                    key={`article_${index}`}
+                    date={article.date}
+                    title={article.title}
+                    imgUrl={article.imgUrl}
+                    articleUrl={article.articleUrl}
+                />
+            ))}
+        </div>
+    </div>
   )
 }
 
