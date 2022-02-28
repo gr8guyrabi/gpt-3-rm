3bf330098549b743145b23c8b66b22ae7d7fe165
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-02-28 19:13:32 +0545

    setup footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,11 +1,93 @@
 import React from 'react'
 
+import gpt3Logo from '../../assets/logo.svg'
 import './footer.css'
 
+const footerData = [
+    {
+        title: 'links',
+        contents: [
+            {
+                name: 'Overons',
+                url: '#'
+            },
+            {
+                name: 'Social Media',
+                url: '#'
+            },
+            {
+                name: 'Counters',
+                url: '#'
+            },
+            {
+                name: 'Contact',
+                url: '#'
+            },
+        ]
+    },
+    {
+        title: 'company',
+        contents: [
+            {
+                name: 'Terms & Conditions',
+                url: '#'
+            },
+            {
+                name: 'Privacy Policy',
+                url: '#'
+            },
+            {
+                name: 'Contact',
+                url: '#'
+            },
+        ]
+    },
+    {
+        title: 'get in touch',
+        contents: [
+            {
+                name: 'Crechterwoord K12 182 DK Alknjkcb',
+            },
+            {
+                name: '085-132567',
+            },
+            {
+                name: 'info@payme.net',
+                url: '#'
+            },
+        ]
+    },
+]
+
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <div className="gpt3__footer section__padding">
+            <div className="gpt3__footer-heading">
+                <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+            </div>
+            <div className="gpt3__footer-btn">
+                <p>Request Early Access</p>
+            </div>
+
+            <div className="gpt3__footer-links">
+                <div className="gpt3__footer-links_logo">
+                    <img src={gpt3Logo} alt="logo" />
+                    <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+                </div>
+                {footerData.map((data, index) => (
+                    <div key={`title_${index}`} className="gpt3__footer-links_div">
+                        <h4>{data.title}</h4>
+                        {data?.contents?.map((content, index) => (
+                            <p key={`subtitle_${data.title}_${index}`}>{ content.url ? <a href={content.url}>{content.name}</a> : content.name }</p>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className="gpt3__footer-copyright">
+                <p>© 2021 GPT-3. All rights reserved.</p>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
